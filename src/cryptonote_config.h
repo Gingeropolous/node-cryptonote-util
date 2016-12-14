913f2c8cc105ba6c3dc7a6b3f87093b8759d13f8
gingeropolous <gingeropolous@gmail.com>
2016-12-13 19:52:04 -0500

    more numbers

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER          0
-#define CURRENT_TRANSACTION_VERSION                     3
-#define CURRENT_BLOCK_MAJOR_VERSION                     1
+#define CURRENT_TRANSACTION_VERSION                     2
+#define CURRENT_BLOCK_MAJOR_VERSION                     4
 #define CURRENT_BLOCK_MINOR_VERSION                     0
 
 #define BLOCK_MAJOR_VERSION_1                           1
